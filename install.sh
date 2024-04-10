59517eaf2b62680346f9059e2ea5e2b3f1324a50
JadeBetty <jadebetty.7261@gmail.com>
2024-04-10 13:44:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,38 @@ debian() {
     echo "installing git"
     sudo apt install git
     echo "yay done"
+    echo "installing fonts"
+    mkdir tempslol
+    cd tempslol
+    sudo apt install wget unzip
+    echo "downloading wget and unzip"
+    wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.0/JetBrainsMono.zip
+    wget https://github.com/be5invis/Iosevka/releases/download/v29.1.0/PkgTTF-Iosevka-29.1.0.zip
+    mv JetBrainsMono-2.304.zip 1
+    mv JetBrainsMono.zip 2
+    mv PkgTTF-Iosevka-29.1.0.zip 3
+    mkdir ttf1
+    unzip 1 -d ttf1
+    cd ttf1/fonts/ttf
+    cp JetBrainsMono-Regular.ttf ~/.local/share/fonts
+    cd ..
+    cd ..
+    cd ..
+    mkdir ttf2
+    unzip 2 -d ttf2
+    cd ttf2
+    cp JetBrainsMonoNerdFont-Regular.ttf ~/.local/share/fonts
+    cd ..
+    mkdir ttf3
+    unzip 3 -d ttf3
+    cd ttf3
+    cp Iosevka-Regular.ttf ~/.local/share/fonts
+    cd ..
+    cd ..
+    rm -r tempslol
+    echo "Done installing fonts"
+    sudo fc-cache -f -v
     echo "overriding current configs"
     stow .
     echo "yay done"
